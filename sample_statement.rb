505ee67f1ce85afc84b60409179b43259713e120
Franklin Bado <fbado66@gmail.com>
2020-07-14 02:58:24 +0000

    Done.

---
+++ sample_statement.rb
@@ -0,0 +1,12 @@
+# chance_of_rain = 0.3 
+# if chance_of_rain <=0.25 
+#   puts "Pack a sun shelter"
+#   elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+#   puts "Pack an umbrella"
+# else 
+#   puts "Stay home and read Hegel"
+# end 
+
+puts "You know what year it is??"
+this_year = Time.now.year
+puts "hey, it's #{this_year}" unless this_year == 2020 
